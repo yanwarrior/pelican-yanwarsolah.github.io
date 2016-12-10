ba7ee1bdb2d4197f481b0933cdb0d5961936ca13
yanwarsolah <yanwarsolah@gmail.com>
2016-12-10 18:01:04 +0700

    Change themes with pelican-blue

--- pelicanconf.py
+++ pelicanconf.py
@@ -15,6 +15,11 @@ PATH = 'content'
 TIMEZONE = 'Asia/Jakarta'
 
 DEFAULT_LANG = 'id'
+THEME = 'pelican-blue'
+SIDEBAR_DIGEST = 'Programmer and Web Developer'
+DISPLAY_PAGES_ON_MENU = True
+TWITTER_USERNAME = 'yanwarsolah'
+MENUITEMS = (('Home', SITEURL),)
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -24,14 +29,12 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+# LINKS = ((),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+			('Facebook', 'https://www.facebook.com/yanwarsolah'),
+			('Github', 'https://github.com/yanwarsolah/'))
 
 DEFAULT_PAGINATION = 10
 
